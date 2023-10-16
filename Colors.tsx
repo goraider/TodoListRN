904d2dc4d282706bf6951f1d78a0f15c1a31bcf4
gosain <alksmaniac@gmail.com>
2023-10-16 12:49:41 -0600

    begining to todolist app

---
+++ Colors.tsx
@@ -0,0 +1,10 @@
+const colors = {
+
+    black:"#2D3436",
+    blue:"#24A6D9",
+    ligthBlue:"#A7CBD9",
+    white:"#FFFFFF"
+}
+
+
+export default colors;
---
+++ tempData.tsx
@@ -0,0 +1,45 @@
+const tempData = [
+
+    {
+        id:90,
+        name: "Plan a Trip",
+        color: "#24A6D9",
+        todos: [
+            {
+                title: "Book Flight",
+                completed: true
+            },
+            {
+                title: "Cook",
+                completed: true
+            },
+            {
+                title: "Program",
+                completed: true
+            }
+        ]
+
+    },
+    {
+        id:91,
+        name: "Errands",
+        color: "#8022D9",
+        todos: [
+            {
+                title: "Buy Milk",
+                completed: false
+            },
+            {
+                title: "Run",
+                completed: true
+            },
+            {
+                title: "Going to Drink",
+                completed: false
+            }
+        ]
+
+    }
+];
+
+export default tempData;
